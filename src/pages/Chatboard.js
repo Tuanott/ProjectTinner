8f727e55b607d6b255f25a919a6357bf03e53b2e
“Tuanott” <patipoltang@gmail.com>
2024-02-28 23:31:12 +0700

    tinnerProject

---
+++ src/pages/Chatboard.js
@@ -0,0 +1,92 @@
+import {useEffect, useState} from 'react'
+import ChatContainer from '../components/ChatContainer'
+import {useCookies} from 'react-cookie'
+import axios from 'axios'
+
+const Dashboard = () => {
+    const [user, setUser] = useState(null)
+    const [genderedUsers, setGenderedUsers] = useState(null)
+    const [lastDirection, setLastDirection] = useState()
+    const [cookies, setCookie, removeCookie] = useCookies(['user'])
+
+    const userId = cookies.UserId
+
+
+    const getUser = async () => {
+        try {
+            const response = await axios.get('http://52.65.52.76:8000/user', {
+                params: {userId}
+            })
+            setUser(response.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    const getGenderedUsers = async () => {
+        try {
+            const response = await axios.get('http://52.65.52.76:8000/gendered-users', {
+                params: {gender: user?.gender_interest}
+            })
+            setGenderedUsers(response.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getUser()
+
+    }, [])
+
+    useEffect(() => {
+        if (user) {
+            getGenderedUsers()
+        }
+    }, [user])
+
+    const updateMatches = async (matchedUserId) => {
+        try {
+            await axios.put('http://localhost:8000/addmatch', {
+                userId,
+                matchedUserId
+            })
+            getUser()
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+
+    const swiped = (direction, swipedUserId) => {
+        if (direction === 'right') {
+            updateMatches(swipedUserId)
+        }
+        setLastDirection(direction)
+    }
+
+    const outOfFrame = (name) => {
+        console.log(name + ' left the screen!')
+    }
+
+    const matchedUserIds = user?.matches.map(({user_id}) => user_id).concat(userId)
+
+    const filteredGenderedUsers = genderedUsers?.filter(genderedUser => !matchedUserIds.includes(genderedUser.user_id))
+    
+
+    console.log('filteredGenderedUsers ', filteredGenderedUsers)
+    return (
+        <>
+            {user &&
+            <div>
+                <div className="chatdashboard">
+                    
+                    <ChatContainer user={user}/>
+                    
+                </div>
+             </div>}
+        </>
+    )
+    
+}
+
+export default Dashboard
---
+++ src/pages/Home.js
@@ -0,0 +1,53 @@
+import Nav from '../components/Nav'
+import AuthModal from "../components/AuthModal"
+import {useState} from 'react'
+import {useCookies} from "react-cookie"
+import logo from '../images/Tinnerlogo.png'
+
+const Home = () => {
+    const [showModal, setShowModal] = useState(false)
+    const [isSignUp, setIsSignUp] = useState(true)
+    const [cookies, setCookie, removeCookie] = useCookies(['user'])
+    const authToken = cookies.AuthToken
+
+    const handleClick = () => {
+        if (authToken) {
+            removeCookie('UserId', cookies.UserId)
+            removeCookie('AuthToken', cookies.AuthToken)
+            window.location.reload()
+            return
+        }
+        setShowModal(true)
+        setIsSignUp(true)
+    }
+
+    return (
+        <div className="overlay">
+            
+            <div className="home">
+                
+                <img src={logo} alt="Logo" width="300px" height="120px" />
+                
+                <h1 className="primary-title">อยากเจอคนที่ฮีลใจ</h1>
+                
+                <Nav
+                authToken={authToken}
+                minimal={false}
+                setShowModal={setShowModal}
+                showModal={showModal}
+                setIsSignUp={setIsSignUp}
+            />
+                <button className="primary-button" onClick={handleClick}>
+                    {authToken ? 'Signout' : 'Create Account'}
+                </button>
+            
+
+
+                {showModal && (
+                    <AuthModal setShowModal={setShowModal} isSignUp={isSignUp}/>
+                )}
+            </div>
+        </div>
+    )
+}
+export default Home
